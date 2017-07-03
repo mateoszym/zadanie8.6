779c2385dc70e468c9b82944a5e176b8ffeee264
mateoszym <mateoszym@gmail.com>
2017-07-03 23:04:21 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 var a = 2;
-var b = 5;
-var value = (a * a) + (2 * a * b) - (b * b);
+    b = 5;
+    value = (a * a) + (2 * a * b) - (b * b);
 
 if (value > 0) {
-    alert('Wynik działania jest dodatni');
     console.log('Wynik działania jest dodatni');
   } else if (value < 0) {
-    alert('Wynik działania jest ujemny');
     console.log('Wynik działania jest ujemny');
   } else {
-  	alert('Wynik jest równy zero');
   	console.log('Wynik jest równy zero');
   }
