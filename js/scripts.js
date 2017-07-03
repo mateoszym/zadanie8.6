4fbcfd4e047ab19dfaacce754a51a59727030548
mateoszym <mateoszym@gmail.com>
2017-07-03 17:40:12 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = 2;
+var b = 5;
+var value = (a * a) + (2 * a * b) - (b * b);
+
+if (value > 0) {
+    alert('Wynik działania jest dodatni');
+    console.log('Wynik działania jest dodatni');
+  } else if (value < 0) {
+    alert('Wynik działania jest ujemny');
+    console.log('Wynik działania jest ujemny');
+  } else {
+  	alert('Wynik jest równy zero');
+  	console.log('Wynik jest równy zero');
+  }
