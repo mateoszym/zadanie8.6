4fb1e03f5d404e559d8e72909fb59896b2cf6879
mateoszym <mateoszym@gmail.com>
2017-07-05 11:55:43 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 var a = 2;
     b = 5;
     value = (a * a) + (2 * a * b) - (b * b);
-
-if (value > 0) {
-    console.log('Wynik działania jest dodatni');
-  } else if (value < 0) {
-    console.log('Wynik działania jest ujemny');
-  } else {
-  	console.log('Wynik jest równy zero');
-  }
+console.log(value > 0 ? 'Wynik działania jest dodatni' : (value < 0) ? 'Wynik działania jest ujemny' : 'Wynik jest równy zero')
