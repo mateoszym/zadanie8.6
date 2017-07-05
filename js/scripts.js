dbe8cca84cb1261e52b03e0c2186d6d621694a00
mateoszym <mateoszym@gmail.com>
2017-07-05 17:53:40 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 var a = 2;
-    b = 5;
-    value = (a * a) + (2 * a * b) - (b * b);
+var b = 5;
+var value = (a * a) + (2 * a * b) - (b * b);
 console.log(value > 0 ? 'Wynik działania jest dodatni' : (value < 0) ? 'Wynik działania jest ujemny' : 'Wynik jest równy zero')
